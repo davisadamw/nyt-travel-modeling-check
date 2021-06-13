c80f473a7e7dbde3c1823f0a8e11fd78cb594ed6
Adam Davis <davisadamw@gmail.com>
2021-06-12 19:57:35 -0700

    cv and correlation process update with figures and chosen model results

--- XX_02_run-bandwidth-assessment.R
+++ XX_02_run-bandwidth-assessment.R
@@ -3,13 +3,15 @@ library(sf)
 library(spgwr)
 source("XX_99_bandwidth-selection-workarounds.R")
 
-# load data
-all_model_data <- read_rds("Data/Intermediates/all_places_prepped_4pts_528.rds")
+chosen_bandwidth <- 3000
 
-prediction_grid_prepped <- read_rds("Data/Intermediates/prediction_grid_prepped_4pts_528.rds") %>% 
+# load data
+all_model_data <- read_rds("Data/all_places_prepped_4pts_528.rds")
+
+prediction_grid_prepped <- read_rds("Data/prediction_grid_prepped_4pts_528.rds") %>% 
   as_Spatial()
 
-log_model_formula <- read_rds("Data/Intermediates/log_model_formula.rds")
+log_model_formula <- read_rds("Data/log_model_formula.rds")
 
 
 # create the model matrix and coords dataset for each model period
@@ -45,15 +47,13 @@ model_cors_and_cvs <- model_frames %>%
          cv_score = pmap_dbl(list(bandwidth = bandwidth, y = y, x = x, coords = coords, weights = weights),
                              compute_cv_score))
 
+
 model_cors <- model_cors_and_cvs %>% 
   select(model_name, bandwidth, cormats) %>% 
   unnest(cormats)
 
+# correlation plots
 model_cors %>% 
-  # filter(var1 != "(Intercept)",
-  #        var2 != "(Intercept)",
-  #        !str_starts(var1, "place_type"),
-  #        !str_starts(var2, "place_type")) %>% 
   mutate(var_pair = paste(model_name, var1, var2)) %>% 
   ggplot(aes(bandwidth, vals, group = var_pair, color = model_name)) +
   geom_line(alpha = 0.5) +
@@ -63,38 +63,61 @@ model_cors %>%
   scale_x_continuous("Bandwidth, km", breaks = seq(1000, 6000, 500), labels = scales::comma) +
   scale_y_continuous("Correlation of GWR Model Coefficients",
                      breaks = seq(-1, 1, 0.2)) +
-  theme_bw()
+  theme_bw() +
+  theme(panel.grid = element_blank())
 
-ggsave("Cors_allpaths.png", width = 6, height = 4, dpi = 450)
+ggsave("Figures/Cors_allpaths.png", width = 6, height = 4, dpi = 450)
 
-# store the modeling results 
-bandwidths_mods %>% 
-  select(model_name, bandwidth, cv_score:worst_cor_pair) %>% 
-  write_rds("Data/Intermediates/bandwidth_ranges.rds", compress = "gz")
+# cv score plots
+model_cors_and_cvs %>% 
+  select(model_name, bandwidth, cv_score) %>% 
+  # attach mean cv score
+  bind_rows(with_groups(., bandwidth, summarize,
+                        model_name = "average", cv_score = mean(cv_score))) %>% 
+  ggplot(aes(bandwidth, cv_score, group = model_name, color = model_name)) +
+  geom_line() +
+  #geom_point(data = ~ with_groups(.x, model_name, slice_min, order_by = cv_score, n = 2)) +
+  geom_text(aes(label = scales::comma(cv_score, accuracy = 1)),
+            nudge_y = 15, 
+            size = 1.5,
+            show.legend = FALSE) +
+  scale_color_manual("Model", values = c("purple", "darkorange", "black")) +
+  scale_x_continuous("Bandwidth, km", breaks = seq(1000, 6000, 500), labels = scales::comma) +
+  scale_y_continuous("CV Score", labels = scales::comma) +
+  theme_bw() +
+  theme(legend.position = c(0.7, 0.7),
+        legend.title = element_blank(),
+        panel.grid = element_blank())
+  
+ggsave("Figures/CV_scores.png", width = 6, height = 4, dpi = 450)
 
-adapt_frac_mods %>% 
-  select(model_name, q, cv_score:worst_cor_pair) %>% 
-  write_rds("Data/Intermediates/adapt_frac_ranges.rds", compress = "gz")
+# store the modeling results for our chosen model
+model_cors_and_cvs %>% 
+  filter(bandwidth == chosen_bandwidth) %>% 
+  select(model_name, model_ests) %>% 
+  deframe() %>% 
+  write_rds(glue::glue("Data/model_estimates{chosen_bandwidth}km.rds"))
+
 
 # run the models on the grid
 model_00s_3000 <- gwr(log_model_formula, 
                       data = model_frames$data[[1]],
                       coords = model_frames$coords[[1]], 
-                      bandwidth = 3500,
-                      # predict = TRUE, 
-                      # fit.points = prediction_grid_prepped, 
+                      bandwidth = chosen_bandwidth,
+                      predict = TRUE, 
+                      fit.points = prediction_grid_prepped, 
                       longlat = TRUE)
 model_10s_3000 <- gwr(log_model_formula, 
                       data = model_frames$data[[2]],
                       coords = model_frames$coords[[2]], 
-                      bandwidth = 3500,
-                      # predict = TRUE, 
-                      # fit.points = prediction_grid_prepped, 
+                      bandwidth = chosen_bandwidth,
+                      predict = TRUE, 
+                      fit.points = prediction_grid_prepped, 
                       longlat = TRUE)
 
 
 model_00s_3000 %>% 
-  write_rds("Data/model_00s_3000km_Feb11.rds", compress = "gz")
+  write_rds(glue::glue("Data/model_00s_{chosen_bandwidth}km.rds"), compress = "gz")
 
 model_10s_3000 %>% 
-  write_rds("Data/model_10s_3000km_Feb11.rds", compress = "gz")
+  write_rds(glue::glue("Data/model_10s_{chosen_bandwidth}km.rds"), compress = "gz")
